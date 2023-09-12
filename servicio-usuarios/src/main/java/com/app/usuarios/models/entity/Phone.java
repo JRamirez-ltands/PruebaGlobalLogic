d4bf02caeff0210ed65c956bb4db068dbc9bf9a8
JRamirez-ltands <85404105+JRamirez-ltands@users.noreply.github.com>
2023-09-12 11:34:41 -0300

    Corrección de un error que me acabo de dar cuenta, por el correo de respuesta al ejercicio enviado
    Este error fue un descuido que se fue en la subida de fuentes, debido a otras pruebas que estuve realizando en el GET de Phones, olvide dejar esta clase a como estaba antes, con la relación ManyToOne (pueden corroborar esto en la relación de Usuario -> Phones donde si esta de OneToMany, de forma consistente).

--- servicio-usuarios/src/main/java/com/app/usuarios/models/entity/Phone.java
+++ servicio-usuarios/src/main/java/com/app/usuarios/models/entity/Phone.java
@@ -8,7 +8,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 
@@ -30,7 +30,7 @@ public class Phone implements Serializable {
 	@Column( length = 5)
 	private String countrycode;
 	
-	@OneToOne
+	@ManyToOne
 	@JoinColumn(name="usuario_id")
 	private User user;
 	
